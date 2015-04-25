aeb9c12f5d4734244b1c9a060927e6e727677db0
Maarten ter Huurne <maarten@treewalker.org>
2015-04-25 21:30:29 +0200

    Removed "gp2x.h" header
    None of the definitions in it were used anymore.

--- src/gmenu2x.cpp
+++ src/gmenu2x.cpp
@@ -18,8 +18,6 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#include "gp2x.h"
-
 #include "background.h"
 #include "cpu.h"
 #include "debug.h"
