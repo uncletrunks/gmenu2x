ce63697129f0e45b34e02112155c8ab5d94384f6
Paul Cercueil <paul@crapouillou.net>
2020-06-24 14:08:33 +0200

    gmenu2x: Don't force 32-bit depth
    When the resolution is specified from CMake, the code was forcing the
    depth to 32-bit.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- src/gmenu2x.cpp
+++ src/gmenu2x.cpp
@@ -213,7 +213,7 @@ GMenu2X::GMenu2X() : input(*this), sc(this)
 	SDL_WM_SetCaption("GMenu2X", nullptr);
 
 #if defined(G2X_BUILD_OPTION_SCREEN_WIDTH) && defined(G2X_BUILD_OPTION_SCREEN_HEIGHT)
-	s = OutputSurface::open(G2X_BUILD_OPTION_SCREEN_WIDTH, G2X_BUILD_OPTION_SCREEN_HEIGHT, 32);
+	s = OutputSurface::open(G2X_BUILD_OPTION_SCREEN_WIDTH, G2X_BUILD_OPTION_SCREEN_HEIGHT, 0);
 #else
 	// find largest resolution available
 	for (const auto res : supported_resolutions)
